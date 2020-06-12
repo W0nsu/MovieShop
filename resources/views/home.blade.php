11dd91e25d5e5d7728ec2268a33289874e6900e7
Wonsu <wonsu1996@gmail.com>
2020-06-13 00:33:14 +0200

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 <div class="row row-cols-1 row-cols-md-3 ml-0 mr-0">
     @foreach ($movies as $movie)
-    <div class="col mb-4 container ">
-      <div class="card h-90 pl-3 pt-3 row-cols-2">
+    <div class="col mb-4 container">
+      <div class="card h-100 pl-3 pt-3 row-cols-2">
         <img src="{{URL::asset('photos/')}}/{{$movie->path}}" class="card-img-top" alt="..." class="img-thumbnail col-sm center" style="width: 200px; height:300px;">
-        <div class="card-body col-sm w-50">
+        <div class="card-body col-sm w-75">
             <h3 class="card-title">{{$movie->title}}</h5>
             <p class="card-text">Category: {{$movie->category}}</p>
             <p class="card-text">Year of production: {{$movie->production_year}}</p>
@@ -34,7 +34,7 @@
                 </div>
               </div>
             </div>
-            <a href="{{ route('add',['id'=> $movie->id])}}" class="btn btn-success pull-right">Add to card</a>
+            <a href="{{ route('add',['id'=> $movie->id])}}" class="btn btn-success">Add to card</a>
         </div>
       </div>
     </div>
